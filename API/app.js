b99d9684280e6e4d7b5bc2154d148dba053485fe
PetrichBF <petrichbf@gmail.com>
2022-03-13 08:39:51 +0100

    előkészület az ügyfél oldalakhoz, keresés

--- API/app.js
+++ API/app.js
@@ -213,3 +213,112 @@ app.route("/belepesek")
         });
     })    
 
+    app.route("/kereses")
+    .get(function(req, res) {
+        const q = "SELECT * FROM ugyfelek WHERE CONCAT(csaladnev,' ', keresztnev) LIKE '%?%'";
+        pool.query(q, [req.params.reszlet], function (error, results) {
+            if (!error) {
+                res.send(results);
+            } else {
+                res.send(error);
+            }
+        });
+    })    
+
+
+    //ügyféllekérések
+
+
+
+
+    //bejelentkezéshez a részek
+
+    function authenticateToken(req, res, next) {
+        const authHeader = req.headers['authorization']
+        const token = authHeader && authHeader.split(' ')[1]
+        if (!token)
+            return res.status(401).send({ message: "Azonosítás szükséges!" })
+        jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+            if (err)
+                return res.status(403).send({ message: "Nincs jogosultsága!" })
+            req.user = user
+            next()
+        })
+    }
+    
+    app.post("/users/login", function (req, res) {
+        const { ugyfelid, password } = req.body;
+        console.log(ugyfelid + " " + password)
+        const q = "SELECT * FROM ugyfelek WHERE ugyfelid = ?";
+        pool.query(q, [ugyfelid],
+            function (error, result) {
+                if (error)
+                    res.status(500).send({ message: "Adatbázis hiba!" });
+                else if (result.length == 0) {
+                    res.status(400).send({ message: "Nincs ilyen azonosítójú felhasználó!" })
+                } else {
+                    user = JSON.parse(JSON.stringify(result[0]));
+                   // if (!bcrypt.compareSync(password, ugyfelek.jelszo))
+                   if (password = ugyfelek.jelszo)
+                        return res.status(401).send({ message: "Hibás jelszó!" })
+                    const token = jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: 3600 })
+                    res.json({ token: token, message: "Sikeres bejelentkezés." })
+                }
+            }
+        )
+    })
+    
+
+    // ügyfél lekérdezései
+
+    app.route("/azonositottugyfel/:ugyfelid")
+    .get(function(req, res) {
+        const q = "SELECT * FROM ugyfelek WHERE ugyfelid = ?";
+        pool.query(q, [req.params.ugyfelid], 
+            function(error, results) {
+            if (!error, results) {
+                res.send(results);
+            } else {
+                res.send(error);
+            }   
+        }); 
+    })
+
+    app.route("/azonositottugyfelbelepesek/:ugyfelid")
+    .get(function(req, res) {
+        const q = "SELECT belepesek.* FROM belepesek, berletek WHERE belepesek.berletid = berletek.berletid AND berletek.ugyfelid = ?";
+        pool.query(q, [req.params.ugyfelid], function (error, results) {
+            if (!error) {
+                res.send(results);
+            } else {
+                res.send(error);
+            }
+        });
+    })    
+
+    app.route("/azonositottervenyesberletek/:ugyfelid")
+        .get(function(req, res) {
+            const q = "SELECT * , DATE_ADD(ervkezdet, INTERVAL ervnapok - 1 DAY) as ervvege FROM berletek WHERE (lehetosegek > 0) && (ervkezdet <= now()) && (DATE_ADD(ervkezdet, INTERVAL ervnapok DAY) > now()) && ugyfelid = ?";
+            pool.query(q, [req.params.ugyfelid], 
+                function (error, results) {
+                if (!error) {
+                    res.send(results);
+                } else {
+                    res.send(error);
+                }
+            });
+        })
+
+
+        app.route("/azonositottberletek/:ugyfelid")
+        .get(function(req, res) {
+            const q = "SELECT * , DATE_ADD(ervkezdet, INTERVAL ervnapok - 1 DAY) as ervvege FROM berletek WHERE ugyfelid = ?";
+            pool.query(q, [req.params.ugyfelid], 
+                function (error, results) {
+                if (!error) {
+                    res.send(results);
+                } else {
+                    res.send(error);
+                }
+            });
+        })
