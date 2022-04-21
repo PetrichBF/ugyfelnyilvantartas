176b48977cdbf832081f345b88c59f2c9a632d71
PetrichBF <petrichbf@gmail.com>
2022-04-21 05:35:13 +0200

    index.js javítása - ügyféllekérdezések

--- USERS/index.js
+++ USERS/index.js
@@ -10,99 +10,6 @@ bejelentkezve = false;
 // - belépéseinek listája
 // - bérleteinek listája
 
-function lekerdezes() {
-    ugyfelAdatok();    
-    belepesekLista();
-    ugyfelBerletekLista();
-} 
-
-//ügyfél bejelentkezése és adatok alap ellenőrzése (legyen ID és jelszó is megadva)
-document.getElementById("bejelentkezes").onclick = function(e) {
-    e.preventDefault(); 
-        if (bejelentkezve) {
-            if (!confirm("Biztosan ki szeretne jelentkezni?")) {
-                return}
-            sessionStorage.clear();
-            bejelentkezve = false;
-            
-            ugyfelid1.value = "";
-            password.value = "";
-            bejelentkezes.innerHTML = "Belépés";
-            alert("Sikeres kijelentkezés")
-            lekerdezes();
-            return;
-        }
-        if (document.getElementById("ugyfelid1").value=="" || 
-            document.getElementById("password").value=="") {
-            alert("Üres ügyfélazonosító vagy jelszó!")
-            return
-        }
-
-        fetch(hoszt+'users/login', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json;charset=utf-8'
-            },
-            body: JSON.stringify({
-                "ugyfelid": document.getElementById("ugyfelid1").value,
-                "password": document.getElementById("password").value
-            })
-        })
-            .then((response) => response.json())
-            .then(json => {
-                alert(json.message)
-                console.log(json.token)
-                if (json.token) //ellenőrzés, hogy sikeres bejelentkezés volt-e
-                    sessionStorage.token = json.token;
-                    sessionStorage.ugyfel = document.getElementById("ugyfelid1").value;
-                    bejelentkezve = true;
-                    bejelentkezes.innerHTML = "Kilépés";
-                    lekerdezes();
-            })
-            .catch(err => console.log(err))    
-}
-
-
-function ugyfelAdatok() {
-    const ugyfelAdatok = document.getElementById("ugyfelAdatok");
-    if (!sessionStorage.ugyfel && !sessionStorage.token) {
-        ugyfelAdatok.innerHTML = "Kérem, jelentkezzen be!";
-        return;
-    }
-    const url= hoszt + 'azonositottugyfel/' + sessionStorage.ugyfel;
-    
-    const token = "Bearer: " + sessionStorage.token;
-    fetch(url, {
-        method: 'GET',
-        headers: {
-            'Authorization' : token
-        }
-    })
-        .then((response) => response.json())
-        .then(json => {
-            console.log(json);
-            ugyfelAdatok.innerHTML="";
-            json.forEach(f => {
-        sor ='Ügyfél azonosítóm: ' + f.ugyfelid +'<br>';
-        sor +='Nevem: ' + f.csaladnev +' '  + f.keresztnev +'<br>';
-        sor +='Születési időm: ' + f.szulido.split("T")[0].toString() +'<br>';
-        sor +='Nemem: '
-        if (f.neme == "F") {sor += 'férfi'};
-        if (f.neme == "N") {sor += 'nő'};
-        sor+='<br>';
-        sor +='E-mail címem: ' + f.email +'<br>';
-        sor +='Címem: ' + f.iranyitoszam +' ';
-        sor +='' + f.telepules + ', ' + f.lakcim +'<br>';
-        sor +='Telefonom: ' + f.telefon +'<br>';
-        sor +='Hírlevelet '
-        if (f.hirlevel == 0) {sor += 'nem '};
-        sor += 'kérek <br>';
-        ugyfelAdatok.innerHTML += sor;
-            });
-        })
-        .catch(err => console.log(err));
-}
-
 //bejelentkezés nélkül is látható
 function berlettipusokLista() {
     const url= hoszt + 'berlettipusok';
@@ -129,15 +36,122 @@ function berlettipusokLista() {
         .catch(err => console.log(err));
 }
 
+
+
+function lekerdezes() {
+
+    ugyfelAdatok();    
+    ugyfelBerletekLista();
+    belepesekLista();
+
+} 
+
+//ügyfél bejelentkezése és adatok alap ellenőrzése (legyen ID és jelszó is megadva)
+document.getElementById("bejelentkezes").onclick = function(e) {
+    e.preventDefault(); 
+        if (bejelentkezve) {
+            if (!confirm("Biztosan ki szeretne jelentkezni?")) {
+                return
+            }
+            sessionStorage.clear();
+            bejelentkezve = false;
+            
+            ugyfelid1.value = "";
+            password.value = "";
+            bejelentkezes.innerHTML = "Belépés";
+            document.getElementById("ugyfelid1").disabled = false;
+            alert("Sikeres kijelentkezés")
+            lekerdezes();
+            return;
+        }
+        if (document.getElementById("ugyfelid1").value == "" || 
+            document.getElementById("password").value == "") {
+            alert("Üres ügyfélazonosító vagy jelszó!")
+            return
+        }
+
+        fetch(hoszt+'users/login', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({
+                "ugyfelid": document.getElementById("ugyfelid1").value,
+                "password": document.getElementById("password").value
+            })
+        })
+            .then((response) => response.json())
+            .then(json => {
+                alert(json.message)
+                if (json.token) {
+                    //ellenőrzés, hogy sikeres bejelentkezés volt-e
+                    sessionStorage.token = json.token;
+                    sessionStorage.ugyfel = document.getElementById("ugyfelid1").value;
+
+                    document.getElementById("ugyfelid1").disabled = true;
+                    password.value = "";
+
+                    bejelentkezve = true;
+                    bejelentkezes.innerHTML = "Kilépés";
+                    lekerdezes();
+                }
+            })
+            .catch(err => console.log(err))    
+}
+
 //csak bejelentkezést követően látható:
 
-function ugyfelBerletekLista() {
-    if (!sessionStorage.ugyfelid) {
+function ugyfelAdatok() {
+    
+    const ugyfelAdatok = document.getElementById("ugyfelAdatok");
+    if (!sessionStorage.ugyfel && !sessionStorage.token) {
+        ugyfelAdatok.innerHTML = "Kérem, jelentkezzen be!";
         return;
     }
-    const ugyfelBerletekLista = document.getElementById("ugyfelBerletekLista");
+    const url= hoszt + 'azonositottugyfel/' + sessionStorage.ugyfel;
+    
     const token = "Bearer: " + sessionStorage.token;
- 
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization' : token
+        }
+    })
+        .then((response) => response.json())
+        .then(json => {            
+            ugyfelAdatok.innerHTML="";        
+            json.forEach(f => {
+            sor ='Ügyfél azonosítóm: ' + f.ugyfelid +'<br>';
+            sor +='Nevem: ' + f.csaladnev +' '  + f.keresztnev +'<br>';
+            sor +='Születési időm: ' + f.szulido.split("T")[0].toString() +'<br>';
+            sor +='Nemem: '
+            if (f.neme == "F") {sor += 'férfi'};
+            if (f.neme == "N") {sor += 'nő'};
+            sor+='<br>';
+            sor +='E-mail címem: ' + f.email +'<br>';
+            sor +='Címem: ' + f.iranyitoszam +' ';
+            sor +='' + f.telepules + ', ' + f.lakcim +'<br>';
+            sor +='Telefonom: ' + f.telefon +'<br>';
+            sor +='Hírlevelet '
+            if (f.hirlevel == 0) {sor += 'nem '};
+            sor += 'kérek <br>';
+            ugyfelAdatok.innerHTML += sor;
+            });
+        })
+        .catch(err => console.log(err));
+}
+
+
+
+
+function ugyfelBerletekLista() {
+    const ugyfelBerletekLista = document.getElementById("ugyfelBerletekLista");
+    if (!sessionStorage.token) {
+        ugyfelBerletekLista.innerHTML="";
+        return;
+    }
+    const token = "Bearer: " + sessionStorage.token;
+    
     ma0 = new Date();
     ma0.setHours(0);
     ma0.setMinutes(0);
@@ -148,7 +162,7 @@ function ugyfelBerletekLista() {
     ma24.setMinutes(59);
     ma24.setSeconds(59);
   
-    url= hoszt + 'azonositottberletek/' + sessionStorage.ugyfel;
+    url= hoszt + 'azonositottberletek/' + sessionStorage.ugyfel;;
     if (document.getElementById("ugyfelervenyes").checked) {
         url = hoszt + 'azonositottervenyesberletek/' + sessionStorage.ugyfel;
     }
@@ -160,6 +174,7 @@ function ugyfelBerletekLista() {
     })
         .then((response) => response.json())
         .then(json => {
+            ugyfelBerletekLista.innerHTML = "";
             ugyfelBerletekLista.innerHTML = 
             "<thead><tr><td>ID</td><td>Bérletnév</td><td>Vásárlás ideje</td><td>Kezdet</td><td>Napok</td><td>Lejárat</td><td>Ár</td><td>Lehetőség</td><td>Állapot</td></tr></thead>"
             
@@ -177,7 +192,7 @@ function ugyfelBerletekLista() {
                 } else {
                 sor += "<td>" + f.lehetosegek + "</td>"
                 }
-                if (lehetoseg = 0) 
+                if (f.lehetosegek == 0) 
                 { sor += "<td>felhasznált</td>" } 
                 else if (Date.parse(f.ervkezdet) > Date.parse(ma24))
                 { sor += "<td>még nem érvényes</td>"} 
@@ -197,17 +212,19 @@ function ugyfelBerletekLista() {
 }
 
 function belepesekLista() {
-    if (!sessionStorage.ugyfelid) {
+    
+    const ugyfelbelepesekLista = document.getElementById("ugyfelbelepesekLista");
+    if (!sessionStorage.ugyfel) {
+        ugyfelbelepesekLista.innerHTML = "";
         return;
     }
+
+    const token = "Bearer: " + sessionStorage.token;
+
     url= hoszt + 'azonositottugyfelbelepesek/' + sessionStorage.ugyfel;
     if (document.getElementById("utolsok").checked) {
         url = hoszt + 'azonositottugyfelbelepesek5/' + sessionStorage.ugyfel;
     }
-
-    const ugyfelbelepesekLista = document.getElementById("ugyfelbelepesekLista");
-    const token = "Bearer: " + sessionStorage.token;
-
 
     fetch(url, {
         method: 'GET',
@@ -217,6 +234,9 @@ function belepesekLista() {
     })
         .then((response) => response.json())
         .then(json => {
+            ugyfelbelepesekLista.innerHTML = "";
+            console.log(sessionStorage.ugyfel);
+
             ugyfelbelepesekLista.innerHTML = 
             "<thead><tr><td>ID</td><td>Bérlet</td><td>Belépés</td><td>Kilépés</td></tr></thead>"
             json.forEach(f => {
